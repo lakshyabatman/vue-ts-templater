05aeb5f25b4475e0bd3ec3aa313dc70747734729
Lakshya Khera <lakshya.khera@gmail.com>
2020-05-16 14:36:03 +0530

    NPM LIB done

---
+++ template/template.component.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>Hello World</p>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import {Component} from 'vue-property-decorator';
+
+@Component({})
+export default class {{name}} extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
